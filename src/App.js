6a1a9a3f62a2971791c88ffd6e3c920ecf4d0a1f
amani <amanidjebbi88@gmail.com>
2021-11-20 22:30:28 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -16,6 +16,17 @@ class App extends Component {
   };
   //Function qui affiche le temps écoulé
   handleClick = () => {
+    if (this.state.intervalId) {
+      clearInterval(this.state.intervalId);
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          intervalId: 0,
+          count: 0,
+        };
+      });
+      return;
+    }
     const newIntervalId = setInterval(() => {
       this.setState((prevState) => {
         return {
@@ -39,33 +50,60 @@ class App extends Component {
       show: !this.state.show,
     });
   };
+  /*******************************/
   render() {
     return (
       <div
         style={{
           backgroundColor: "grey",
-          width: "500px",
+          width: "400px",
           padding: "50px",
-          margin: "50px auto",
+          margin: "20px auto",
+          borderRadius: "200px",
         }}
       >
         {this.state.show && (
           <div>
-            <h1 style={{ color: "purple" }}>{this.state.Person.fullName}</h1>
-            <h2 style={{ color: "purple" }}>{this.state.Person.bio}</h2>
-            <img src={this.state.Person.imgSrc} alt="ma photo"></img>
-            <h3 style={{ color: "purple" }}>{this.state.Person.profession}</h3>
-            <h3>le Cycle de vie est de {this.state.count} secondes</h3>
+            <h1 style={{ color: "purple", textAlign: "center" }}>
+              {this.state.Person.fullName}
+            </h1>
+            <h2 style={{ color: "purple", textAlign: "center" }}>
+              {this.state.Person.bio}
+            </h2>
+            <img
+              src={myPhoto}
+              alt="ma photo"
+              style={{
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: "200px",
+              }}
+            ></img>
+            <h3 style={{ color: "purple", textAlign: "center" }}>
+              {this.state.Person.profession}
+            </h3>
+            <h3 style={{ textAlign: "center", color: "purple" }}>
+              the life cycle= <input type="text" value={this.state.count} />{" "}
+              secondes
+            </h3>
 
             <br />
             <br />
           </div>
         )}
         <button
-          style={{ color: "purple", backgroundColor: "pink", width: "150px" }}
+          style={{
+            color: "purple",
+            backgroundColor: "pink",
+            width: "150px",
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
           onClick={() => {
-            this.handleShowPerson;
-            this.handleClick;
+            this.handleShowPerson();
+            this.handleClick();
           }}
           className="btn"
         >
